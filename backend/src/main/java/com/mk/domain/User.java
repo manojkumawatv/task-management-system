09666aa5e884e1dbcfde1163b0ec57c2a3977015
manojkumawatv <manojkumawatv@gmail.com>
2024-01-27 00:38:01 +0530

    setup done for persistent layer entity, repo and dto

---
+++ backend/src/main/java/com/mk/domain/User.java
@@ -0,0 +1,52 @@
+package com.mk.domain;
+
+import java.time.LocalDate;
+
+import com.mk.dto.UserDTO;
+
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@Column(name="user_id")
+	private Long userId;
+	private String username;
+	@Column(name="email_id")
+	private String emailId;
+	private String password;
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void prepareUserEntity(UserDTO user) {
+		this.userId=user.getUserId();
+		this.username=user.getUsername();
+		this.emailId=user.getEmailId();
+		this.password=user.getUsername();
+	}
+}
---
+++ backend/src/main/java/com/mk/dto/UserDTO.java
@@ -0,0 +1,32 @@
+package com.mk.dto;
+
+public class UserDTO {
+	private Long userId;
+	private String username;
+	private String emailId;
+	private String password;
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
