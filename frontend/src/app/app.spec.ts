81d693b6c77e397e48c361995a066688013a50a0
manojkumawatv <manojkumawatv@gmail.com>
2025-09-06 23:59:00 +0530

    git

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,11 +1,12 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+  imports: [App, HttpClientModule],
       providers: [provideZonelessChangeDetection()]
     }).compileComponents();
   });
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,24 +1,37 @@
-import { Component, signal, OnInit, NgModule} from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [CommonModule, FormsModule]
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('frontend');
   tasks: any[] = [];
-  newTaskName: String = '';
-  constructor(private http: HttpClient){}
-  ngOnInit(){
+  newTaskName: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
     this.fetchTasks();
   }
+
   fetchTasks() {
     this.http.get<any[]>('http://localhost:8765/tasks').subscribe(data => {
       this.tasks = data;
     });
+  }
+
+  addTask() {
+    if (!this.newTaskName.trim()) return;
+    // Optionally, send to backend here
+    this.tasks.push({ name: this.newTaskName });
+    this.newTaskName = '';
   }
 }
